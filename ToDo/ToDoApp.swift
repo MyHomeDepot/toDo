de5a855acab92538887e79bffe6a3d49ed1b16a9
levelgitpersonal <levelgitpersonal@gmail.com>
2024-07-29 17:07:49 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by LEO on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
