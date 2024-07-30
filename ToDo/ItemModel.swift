3c911316c9ea92c63d8924b11636644bc08e6917
levelgitpersonal <levelgitpersonal@gmail.com>
2024-07-30 15:12:26 +0300

    Start point

---
+++ ToDo/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  ToDo
+//
+//  Created by LEO on 29.07.2024.
+//
+
+import Foundation
---
+++ ToDo/ListModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListModel.swift
+//  ToDo
+//
+//  Created by LEO on 30.07.2024.
+//
+
+import Foundation
